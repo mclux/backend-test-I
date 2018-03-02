119b17414ab44124a89a5d46db502f47b89c056f
mclux <halexatom2@gmail.com>
2018-03-02 21:16:20 +0100

    init commit

---
+++ DevCenterBot/DevCenterBot/ExcelHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace DevCenterBot
+{
+    public class ExcelHelper
+    {
+        public void ExportExcel(List<ExcelUserVM> input)
+        {
+            Microsoft.Office.Interop.Excel.Application excelApp = new Excel.Application();
+
+            //Create an Excel workbook instance and open it from the predefined location
+            Excel.Workbook excelWorkBook = (Excel.Workbook)(excelApp.Workbooks.Add(Missing.Value));
+            Excel.Worksheet excelWorkSheet = excelWorkBook.Sheets.Add();
+            excelWorkSheet.Name = "Extracted Details";
+            for (int rows = 1; rows <= input.Count; rows++)
+            {
+                if (rows == 1)
+                {
+                    excelWorkSheet.Cells[1, 1] = "Name";
+                    excelWorkSheet.Cells[1, 2] = "Following Count";
+                }
+                
+                excelWorkSheet.Cells[(rows + 1), 1] = input[rows].Name;
+                excelWorkSheet.Cells[(rows + 1), 2] = input[rows].FollowerCount;
+                
+            }
+            excelWorkBook.SaveAs(System.IO.Directory.GetCurrentDirectory() + @"\DcUsers.xlsx", Excel.XlFileFormat.xlOpenXMLWorkbook, Missing.Value,
+                                    Missing.Value, false, false, Excel.XlSaveAsAccessMode.xlNoChange,
+                                    Excel.XlSaveConflictResolution.xlUserResolution, true,
+                                    Missing.Value, Missing.Value, Missing.Value);
+            excelWorkBook.Close();
+            excelApp.Quit();
+        }        
+    }
+
+    public class ExcelUserVM
+    {
+        public string Name { get; set; }
+        public int FollowerCount { get; set; }
+    }
+}
---
+++ DevCenterBot/DevCenterBot/Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Script.Serialization;
+using System.Xml.Linq;
+using System.Security.Cryptography;
+using LinqToTwitter;
+using Spring.Social.Twitter.Api.Impl;
+using Spring.Social.Twitter.Api;
+using Newtonsoft.Json;
+
+namespace DevCenterBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwitterHelper tw = new TwitterHelper();
+
+            Console.Write("[+]Enter hashtag:");
+            string hastTag = Console.ReadLine();
+            Console.WriteLine("[+]searching tweets...");
+
+            var response = tw.GetTweets(hastTag, 100);
+            var obj = JObject.Parse(response);
+            List<ExcelUserVM> lists = new List<ExcelUserVM>();
+            foreach (var child in obj["statuses"].Children())
+            {
+                var childObj = child.First().Parent;
+                var name = childObj["user"]["name"].ToString();
+                var followerCount = childObj["user"]["followers_count"].ToString();
+                lists.Add(new ExcelUserVM
+                {
+                    Name = name,
+                    FollowerCount =Convert.ToInt32(followerCount)
+                });
+                Console.WriteLine("[+]Profile Name: {0}\n[+]No. of Followers:{1}",name, followerCount);
+                Console.WriteLine("------------------------------------------");                               
+            }
+            ExcelHelper ex = new ExcelHelper();
+            ex.ExportExcel(lists);
+            Console.Read();            
+        }        
+    }
+    
+}
